42890a8b04aac336db3ecafd7d33b4369b093c10
felics <felics6@gmail.com>
2024-07-25 20:32:19 +0300

    Added method, class and set,get.

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,28 @@
 public class Book {
+
+    private String theTitleOfTheBook;
+    private int yearOfPublication;
+    private Author author;
+
+    public Book (String theTitleOfTheBook, int yearOfPublication, Author author){
+        this.theTitleOfTheBook = theTitleOfTheBook;
+        this.yearOfPublication = yearOfPublication;
+        this.author = author;
+    }
+
+    public String getTheTitleOfTheBook() {
+        return this.theTitleOfTheBook;
+    }
+
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+
+    public Author getAuthor(){
+        return this.author;
+    }
+
+    public void setYearOfPublication(int yearOfPublication){
+        this.yearOfPublication = yearOfPublication;
+    }
 }
--- src/LibraryReference.java
+++ src/LibraryReference.java
@@ -1,2 +1,17 @@
 public class LibraryReference {
+
+    public static void main(String[] args) {
+        Author author1 = new Author("George", "Martin");
+        Author author2 = new Author("John", "Tolkien");
+
+        Book book1 = new Book("A Game of Thrones.", 1996, author1);
+        Book book2 = new Book("The Lord of the Rings.", 1954, author2);
+
+        book1.setYearOfPublication(1998);
+
+        System.out.println("Книга 1: " + book1.getTheTitleOfTheBook() + " Автор: " + book1.getAuthor().getFirstName()
+                + " " + book1.getAuthor().getLastName() + ". " + "Год издания: " + book1.getYearOfPublication());
+        System.out.println("Книга 2: " + book2.getTheTitleOfTheBook() + " Автор: " + book2.getAuthor().getFirstName()
+                + " " + book2.getAuthor().getLastName() + ". " + "Год издания: " + book2.getYearOfPublication());
+    }
 }
